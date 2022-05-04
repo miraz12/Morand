38503ee18212ef7630434e61e50a574ea201250c
Anton Christoffersso <anton_christoffersson@hotmail.com>
2022-05-04 21:39:22 +0200

    init commit

---
+++ morand.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import csv
+from random import *
+
+with open('goodmorning.csv', newline='') as csvfile:
+    morns = csv.DictReader(csvfile, delimiter=',')
+    iterator = 0
+    randomLang = randint(1, 271)
+    for row in morns:
+        iterator = iterator + 1
+        if iterator == randomLang:
+            print(row['Language'], ": \n", row['Morning greetings'], "\n\n")
