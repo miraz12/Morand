f88888c94e6ed473dfed935b27d77b27713dbbd1
Anton Christoffersson <anton.christoffersson@surgicalscience.com>
2024-04-16 07:57:17 +0200

    update

--- morand.py
+++ morand.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import csv
-from random import *
+from random import randint
 
-with open('goodmorning.csv', newline='') as csvfile:
-    morns = csv.DictReader(csvfile, delimiter=',')
+with open("goodmorning.csv", newline="") as csvfile:
+    morns = csv.DictReader(csvfile, delimiter=",")
     iterator = 0
     randomLang = randint(1, 271)
     for row in morns:
         iterator = iterator + 1
         if iterator == randomLang:
-            print(row['Language'], ": \n", row['Morning greetings'], "\n\n")
+            print(row["Language"], ": \n", row["Morning greetings"], "\n\n")
